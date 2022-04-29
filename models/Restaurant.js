bf0d1b55464c7b490cb89d3050441710dc373260
puiza <saran.swpy@gmail.com>
2022-04-30 00:51:22 +0700

    add Restaurant model

---
+++ models/Restaurant.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, "Please add a name"]
+    },
+    address: {
+        type: String,
+        require: [true, "Please add restaurant's address"]
+    },
+    telephone: {
+        type: String,
+        require: [true, "Please add telephone number"],
+        match: [/^((\+66|0)(\d{9}))$/, "Please add a valid telephone number"]
+    },
+    openTime: {
+        type: String,
+        default: "8.00"
+    },
+    closeTime: {
+        type: String,
+        default: "21.00"
+        //require: [true, "Please add closing time"]
+    }
+})
+
+module.exports = mongoose.model("Restaurant", RestaurantSchema);
