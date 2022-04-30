e3d0a56cac3a56018802f21b8e8112e75d60813e
puiza <saran.swpy@gmail.com>
2022-04-30 16:51:29 +0700

    add no more than 3 reservation

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -132,6 +132,16 @@ exports.deleteReservation = async (req, res, next) => {
 
 exports.createReservation = async (req, res, next) => {
     try {
+        const reservations = await Reservation.find({
+            user: req.user.id
+        });
+        if (reservations.length >= 3) {
+            return res.status(400).json({
+                success: false,
+                msg: `User cannot have more than 3 reservation`
+            })
+        }
+        
         const { restaurant, date } = req.body;
 
         const reservation = await Reservation.create({
