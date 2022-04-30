3b38c1d03bf0e6a83afc0cfdbea523928f8b9329
KonKinPak <tiewclub9@gmail.com>
2022-04-30 16:51:23 +0700

    revise create Reservation

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -1,155 +1,168 @@
 const Reservation = require("../models/Reservation");
+const Restaurant = require("../models/Restaurant");
 
 exports.findAllReservations = async (req, res, next) => {
-    try {
-        const reservations = await Reservation.find();
-        
-        // if(req.user.role !== "admin") {
-        //     return res.status(401).json({
-        //         success: false,
-        //         msg: "Users are not authorize"
-        //     })
-        // }
+  try {
+    const reservations = await Reservation.find();
 
-        if(!reservations) {
-            return res.status(404).json({
-                success: false,
-                msg: "reservations not found"
-            })
-        }
+    // if(req.user.role !== "admin") {
+    //     return res.status(401).json({
+    //         success: false,
+    //         msg: "Users are not authorize"
+    //     })
+    // }
 
-        return res.status(200).json({
-            success: true,
-            data: reservations
-        })
-
-    } catch (err) {
-        console.log(err.stack);
-        return res.status(400).json({
-            success: false,
-            data: err.message
-        })
+    if (!reservations) {
+      return res.status(404).json({
+        success: false,
+        msg: "reservations not found",
+      });
     }
-}
+
+    return res.status(200).json({
+      success: true,
+      data: reservations,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(400).json({
+      success: false,
+      data: err.message,
+    });
+  }
+};
 
 exports.findMyReservations = async (req, res, next) => {
-    try {
-        const reservations = await Reservation.find({
-            user: req.user.id
-        })
+  try {
+    const reservations = await Reservation.find({
+      user: req.user.id,
+    });
 
-        if (!reservations) {
-            return res.status(404).json({
-                success: false,
-                msg: "reservations not found"
-            })
-        }
-
-        return res.status(200).json({
-            success: true,
-            data: reservations
-        })
-
-    } catch (err) {
-        console.log(err.stack);
-        return res.status(400).json({
-            success: false,
-            data: err.message
-        })
+    if (!reservations) {
+      return res.status(404).json({
+        success: false,
+        msg: "reservations not found",
+      });
     }
-}
+
+    return res.status(200).json({
+      success: true,
+      data: reservations,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(400).json({
+      success: false,
+      data: err.message,
+    });
+  }
+};
 
 exports.updateReservation = async (req, res, next) => {
-    try {
-        let reservation = await Reservation.findById(req.params.id);
+  try {
+    let reservation = await Reservation.findById(req.params.id);
 
-        if(!reservation) {
-            return res.status(404).json({
-                success: false,
-                msg: `Reservation with ID: ${req.params.id} not found`
-            })
-        }
-
-        if(req.user.id !== reservation.user.toString() && req.user.role !== "admin") {
-            return res.status(401).json({
-                success: false,
-                msg: `This User is not authorize to update this reservation`
-            })
-        }
-        
-        reservation = await Reservation.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-
-        return res.status(200).json({
-            success: true,
-            data: reservation
-        }) 
-
-    } catch (err) {
-        console.log(err.stack);
-        return res.status(500).json({
-            success: false,
-            msg: `Cannot update a reservation`
-        })
+    if (!reservation) {
+      return res.status(404).json({
+        success: false,
+        msg: `Reservation with ID: ${req.params.id} not found`,
+      });
     }
-}
+
+    if (
+      req.user.id !== reservation.user.toString() &&
+      req.user.role !== "admin"
+    ) {
+      return res.status(401).json({
+        success: false,
+        msg: `This User is not authorize to update this reservation`,
+      });
+    }
+
+    reservation = await Reservation.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: reservation,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(500).json({
+      success: false,
+      msg: `Cannot update a reservation`,
+    });
+  }
+};
 
 exports.deleteReservation = async (req, res, next) => {
-    try {
-        const reservation = await Reservation.findById(req.params.id);
+  try {
+    const reservation = await Reservation.findById(req.params.id);
 
-        if(!reservation) {
-            return res.status(404).json({
-                success: false,
-                msg: `Reservation not found`
-            })
-        }
-
-        if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin') {
-            return res.status(401).json({
-                success: false,
-                msg: `This user is unauthorize to delete this reservation`
-            })
-        }
-
-        await reservation.remove();
-
-        return res.status(200).json({
-            success: true,
-            data: {}
-        })
-
-    } catch (err) {
-        console.log(err.stack);
-        return res.status(500).json({
-            success: false,
-            msg: `Cannot delete a reservation`
-        })
+    if (!reservation) {
+      return res.status(404).json({
+        success: false,
+        msg: `Reservation not found`,
+      });
     }
-}
+
+    if (
+      reservation.user.toString() !== req.user.id &&
+      req.user.role !== "admin"
+    ) {
+      return res.status(401).json({
+        success: false,
+        msg: `This user is unauthorize to delete this reservation`,
+      });
+    }
+
+    await reservation.remove();
+
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(500).json({
+      success: false,
+      msg: `Cannot delete a reservation`,
+    });
+  }
+};
 
 exports.createReservation = async (req, res, next) => {
-    try {
-        const { restaurant, date } = req.body;
+  try {
+    const { restaurantId, date, status, ntable } = req.body;
 
-        const reservation = await Reservation.create({
-            user: req.user.id,
-            restaurant,
-            date
-        })
+    const restaurant = await Restaurant.findById(restaurantId);
 
-        return res.status(201).json({
-            success: false,
-            data: reservation
-        })
-
-    } catch (err) {
-        console.log(err.stack);
-        return res.status(400).json({
-            success: false,
-            data: err.message
-        })
+    if (!restaurant) {
+      return res.status(404).json({
+        success: false,
+        msg: `Restaurant not found`,
+      });
     }
-}
+
+    const reservation = await Reservation.create({
+      user: req.user.id,
+      restaurant: restaurantId,
+      date,
+      status,
+      ntable,
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: reservation,
+    });
+  } catch (err) {
+    console.log(err.stack);
+    return res.status(400).json({
+      success: false,
+      data: err.message,
+    });
+  }
+};
