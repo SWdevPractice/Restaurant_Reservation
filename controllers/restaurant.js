b11661efcf4c4c97ceed4311e8f4597b2b1b9b93
puiza <saran.swpy@gmail.com>
2022-04-30 17:19:30 +0700

    add create restaurant

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -2,18 +2,6 @@ const Restaurant = require("../models/Restaurant");
 
 exports.findAllRestaurants = async (req, res, next) => {
     try {
-        let query;
-
-        const reqQuery = {...req.query};
-
-        //exclude fields
-        const removeFields = ['select', 'sort', 'page', 'limit'];
-
-        removeFields.forEach(param => {
-            delete reqQuery[param];
-        });
-
-
         const restaurants = await Restaurant.find();
         if(!restaurants) {
             res.status(400).json({
@@ -62,3 +50,36 @@ exports.findRestaurantById = async (req, res, next) => {
     }
 }
 
+exports.createRestaurant = async (req, res, next) => {
+    try {
+        const { name, address, telephone, openTime, closeTime, ntable } = req.body;
+
+        const restaurant = await Restaurant.create({
+            name,
+            address,
+            telephone,
+            openTime,
+            closeTime,
+            ntable
+        })
+
+        if(!restaurant) {
+            res.status(500).json({
+                success: false,
+                msg: `Cannot create a restaurant`
+            })
+        }
+
+        res.status(201).json({
+            success: true,
+            data: restaurant
+        })
+    
+    } catch (err) {
+        console.log(err.stack);
+        return res.status(400).json({
+            success: false,
+            data: err.msg
+        })
+    }
+   }
